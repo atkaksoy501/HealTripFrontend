9d0b509c7060f2ddfc78c47b0543f9c64bc0dab4
boerten <boerten@hotech.club>
2024-03-07 19:44:36 +0300

    create front end project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {Navi} from './components/Navi'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css'
+import { Footer } from './components/Footer';
+function App() {
+  return (
+    <div className='App'>
+      <Navi></Navi>
+      <h1>buba</h1>
+      <h1>buba</h1>
+      <h1>buba</h1>
+      <h1>buba</h1>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navi.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Navbar.css";
+import HealingIcon from "@mui/icons-material/Healing";
+import {
+  IconButton,
+  Typography,
+  useMediaQuery,
+  Stack,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export const Navi = () => {
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div>
+      <Navbar className="navbar">
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <IconButton
+            size="large"
+            edge="start"
+            style={{ color: "#fff", left:"10px"}}
+            aria-label="logo"
+          >
+            <HealingIcon />
+          </IconButton>
+          <div style={{ marginLeft: "10px" }}> {/* Apply margin here */}
+            <Typography
+              variant="h6"
+              component="div"
+              className="navbar-header"
+              sx={{ flexGrow: 1 }}
+            >
+              <a href="#">Heal Trip</a>
+            </Typography>
+          </div>
+        </div>
+        {isMobile ? (
+          <>
+            {menuOpen ? (
+              <MenuIcon onClick={toggleMenu} style={{ color: "#fff" }} />
+            ) : (
+              <MenuIcon onClick={toggleMenu} style={{ color: "#fff" }} />
+            )}
+
+            {menuOpen && (
+              <Collapse isOpen={isMobile} navbar>
+                <Nav className="me-auto" navbar>
+                  <NavItem>
+                    <NavLink href="#" className="navlink">
+                      Treatments
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      className="navlink"
+                      href="https://github.com/reactstrap/reactstrap"
+                    >
+                      About Us
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="navlink" href="#">
+                      Patient Guide
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="navlink" href="#">
+                      Login
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            )}
+          </>
+        ) : (
+          <Stack direction="row" spacing={2}>
+            <Button className="navlink">Treatments</Button>
+            <Button className="navlink">About Us</Button>
+            <Button className="navlink">Patient Guide</Button>
+            <Button className="navlink">Login</Button>
+          </Stack>
+        )}
+      </Navbar>
+    </div>
+  );
+};
