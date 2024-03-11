cb4836c4f4bfea94b56659576ac7582d88266f3b
burakonurerten <burakonurerten@gmail.com>
2024-03-11 18:25:09 +0300

    add section to main page

---
+++ src/pages/MainPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./MainPage.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Button } from "reactstrap";
+
+export const MainPage = () => {
+  return (
+    <div class="banner">
+      <div class="container">
+        <h1>WELCOME TO HEAL TRIP</h1>
+        <p>
+          We are team of talented doctors providing healthcare services with
+          advance technologies.
+        </p>
+        <br></br>
+        <Button className="button" style={{ backgroundColor: '#3c879e', fontSize:"18px", borderRadius: "30px", width:"200px", fontWeight:"bold"}}><a>Learn More</a></Button>
+      </div>
+    </div>
+  );
+};
