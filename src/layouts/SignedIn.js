512dd879a34a3fded72ac9c4e946df16eff615bf
burakonurerten <burakonurerten@gmail.com>
2024-03-11 17:26:35 +0300

    change color of user and add to toggle

--- src/layouts/SignedIn.js
+++ src/layouts/SignedIn.js
@@ -7,7 +7,7 @@ export default function SignedIn({signOut}) {
     <div>
       <Menu.Item style={{marginRight: '2.5em'}}>
         <Image avatar spaced="right" src={avatar}></Image>
-        <Dropdown pointing="top left" text="User Name">
+        <Dropdown pointing="top left" text="User Name" style={{ color: "#fff" }}>
           <Dropdown.Menu>
             <Dropdown.Item text="My Profile" icon="info" />
             <Dropdown.Item onClick={signOut} text="Sign Out" icon="sign-out" />
