693f2cad0dd43d951002a3b30622290b65586c31
burakonurerten <burakonurerten@gmail.com>
2024-03-12 18:09:05 +0300

    remove old gallery

--- src/pages/MyGallery.js
+++ src/pages/MyGallery.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
-
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
 import "./MyGallery.css";
 import photo1 from "../images/gallery-1.jpg";
 import photo2 from "../images/gallery-2.jpg";
@@ -11,51 +10,38 @@ import photo6 from "../images/gallery-6.jpg";
 import photo7 from "../images/gallery-7.jpg";
 import photo8 from "../images/gallery-8.jpg";
 
-
 export const MyGallery = () => {
-    const data = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8]
-  const [slide, setSlide] = useState(0);
+  const data = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8];
 
-  const nextSlide = () => {
-    setSlide(slide === data.length - 1 ? 0 : slide + 1);
-  };
-
-  const prevSlide = () => {
-    setSlide(slide === 0 ? data.length - 1 : slide - 1);
-  };
   return (
-    <div className="burak">
-      <h1 className="title">GALLERY</h1>
-      <div className="carousel">
-    <BsArrowLeftCircleFill onClick={prevSlide} className="arrow arrow-left" />
-    {data.map((item, idx) => {
-      return (
-        <img
-          src={item}
-          alt={item}
-          key={idx}
-          className={slide === idx ? "slide" : "slide slide-hidden"}
-        />
-      );
-    })}
-    <BsArrowRightCircleFill
-      onClick={nextSlide}
-      className="arrow arrow-right"
-    />
-    <span className="indicators">
-      {data.map((_, idx) => {
-        return (
-          <button
-            key={idx}
-            className={
-              slide === idx ? "indicator" : "indicator indicator-inactive"
-            }
-            onClick={() => setSlide(idx)}
-          ></button>
-        );
-      })}
-    </span>
-  </div></div>
-
+    <div style={{ backgroundColor: "#3c879e", height: "100%", padding:"40px 0"}}>
+        <h1 style={{textAlign:"center", color:"#fff", padding:"4px"}}>GALLERY</h1>
+      <Carousel
+        data-bs-theme="primary"
+        style={{margin: "0 auto", width:"90%"}}
+      >
+        <Carousel.Item>
+        <div className="d-flex justify-content-center">
+        <img className="d-block w-25" src={data[0]} alt="First slide" />
+        <img className="d-block w-25" src={data[1]} alt="Second slide" />
+        <img className="d-block w-25" src={data[2]} alt="Third slide" />
+      </div>
+        </Carousel.Item>
+        <Carousel.Item>
+        <div className="d-flex justify-content-center">
+        <img className="d-block w-25" src={data[3]} alt="Second slide" />
+        <img className="d-block w-25" src={data[4]} alt="Second slide" />
+        <img className="d-block w-25" src={data[5]} alt="Second slide" />
+        </div>
+        </Carousel.Item>
+        <Carousel.Item>
+        <div className="d-flex justify-content-center">
+          <img className="d-block w-25" src={data[6]} alt="Third slide" />
+          <img className="d-block w-25" src={data[7]} alt="Third slide" />
+          <img className="d-block w-25" src={data[3]} alt="Third slide" />
+        </div>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 };
