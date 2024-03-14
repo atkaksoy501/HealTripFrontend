e83a28fa5cb9de1ca009ea32199c8c7c8dc5ad00
burakonurerten <burakonurerten@gmail.com>
2024-03-14 18:39:53 +0300

    add emergency section to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,14 @@ import { MainPage } from "./MainPage";
 import { MyGallery } from "./MyGallery";
 import ExperienceSection from "./ExperienceSection";
 import { BlogSectionInHome } from "./BlogSectionInHome";
+import { EmergencySection } from "./EmergencySection";
 
 export default function Home() {
   return (
     <div>
       <MainPage />
       <MyGallery/>
-      <h1>Ömür Baba</h1>
-      <h1>Ömür Baba</h1>
-      <h1>Ömür Baba</h1>
+      <EmergencySection></EmergencySection>
       <ExperienceSection></ExperienceSection>
       <BlogSectionInHome></BlogSectionInHome>
     </div>
