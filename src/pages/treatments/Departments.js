a75142def2eab886d30df2aee1f212f176ec40f9
Aziz Alp Yolcu <azizalpyolcu09@outlook.com>
2024-03-18 15:44:02 +0300

    department and treatment list images

--- src/pages/treatments/Departments.js
+++ src/pages/treatments/Departments.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import { MenuItem, Menu } from 'semantic-ui-react'
+import React, { useState, useEffect} from 'react'
+import { Menu,DropdownMenu,
+  DropdownItem,
+  Dropdown } from 'semantic-ui-react'
+import DepartmentService from '../../services/departmentService';
 
 export default function Departments() {
+
+  const [departments, setDepartments] = useState([]);
+
+  useEffect(()=>{
+    let departmentService = new DepartmentService()
+    departmentService.getDepartments().then(result=>setDepartments(result.data))
+  },[])
+
   return (
     <div>
-      <Menu pointing vertical>
-        <MenuItem name="home" />
-        <MenuItem name="messages" />
-        <MenuItem name="friends" />
+      <Menu vertical>
+        {departments.map((department) => (
+          //<MenuItem key={department.id}>{department.departmentName}</MenuItem>
+          <Dropdown item key={department.id} text={department.departmentName}>
+            <DropdownMenu>
+              <DropdownItem>English</DropdownItem>
+              <DropdownItem>Russian</DropdownItem>
+              <DropdownItem>Spanish</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>   
+        ))}
       </Menu>
     </div>
   )
--- src/pages/treatments/Treatments.js
+++ src/pages/treatments/Treatments.js
@@ -1,18 +1,10 @@
 import React, { useState, useEffect} from 'react'
-import {
-  TableRow,
-  TableHeaderCell,
-  TableHeader,
-  TableFooter,
-  TableCell,
-  TableBody,
-  MenuItem,
-  Icon,
-  Label,
-  Menu,
-  Table,
-} from 'semantic-ui-react'
+import { GridColumn, Grid, CardHeader,
+  CardDescription,
+  CardContent,
+  Card } from 'semantic-ui-react'
 import TreatmentService from '../../services/treatmentService'
+import ImageComponentFromBase64 from './ImageComponentFromBase64';
 
 export default function Treatments() {
 
@@ -21,48 +13,23 @@ export default function Treatments() {
   useEffect(()=>{
     let treatmentService = new TreatmentService()
     treatmentService.getTreatments().then(result=>setTreatments(result.data))
-  })
+  },[])
 
   return (
     <div>
-      <Table celled>
-        <TableHeader>
-          <TableRow>
-            <TableHeaderCell>Treatment</TableHeaderCell>
-            <TableHeaderCell>Description</TableHeaderCell>
-            <TableHeaderCell>Header</TableHeaderCell>
-          </TableRow>
-        </TableHeader>
-
-        <TableBody>
-          {treatments.map((treatment) => (
-            <TableRow key={treatment.id}>
-              <TableCell>{treatment.retreat_name}</TableCell>
-              <TableCell>{treatment.description}</TableCell>
-              <TableCell>Cell</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-
-        <TableFooter>
-          <TableRow>
-            <TableHeaderCell colSpan="3">
-              <Menu floated="right" pagination>
-                <MenuItem as="a" icon>
-                  <Icon name="chevron left" />
-                </MenuItem>
-                <MenuItem as="a">1</MenuItem>
-                <MenuItem as="a">2</MenuItem>
-                <MenuItem as="a">3</MenuItem>
-                <MenuItem as="a">4</MenuItem>
-                <MenuItem as="a" icon>
-                  <Icon name="chevron right" />
-                </MenuItem>
-              </Menu>
-            </TableHeaderCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
+      <Grid columns={3}>
+        {treatments.map((treatment) => (
+          <GridColumn key={treatment.id}>
+            <Card>
+              <ImageComponentFromBase64 base64String={treatment.image.image}></ImageComponentFromBase64>
+              <CardContent>
+                <CardHeader>{treatment.retreat_name}</CardHeader>
+                <CardDescription>{treatment.description}</CardDescription>
+              </CardContent>
+            </Card>
+          </GridColumn>
+        ))}
+      </Grid>
     </div>
   );
 }
