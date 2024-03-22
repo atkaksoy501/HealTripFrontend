101047c74908cf16b6182468957f74c7ea6f666c
Aziz Alp Yolcu <azizalpyolcu09@outlook.com>
2024-03-22 21:21:15 +0300

    styling contact page and fixed map

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -7,21 +7,24 @@ import {
   Button,
   Form,
   ButtonGroup,
-  GridRow, GridColumn, Grid,
 } from "semantic-ui-react";
 import photo1 from "./contact.jpeg";
 import "./Contact.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import MapContainer from "./MapContainer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMapMarkerAlt,
+  faPhone,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { Container, Row, Col } from "reactstrap";
 
 export default function Contact() {
   return (
     <div
       class="contact"
       style={{
-        marginTop: "59px"
+        marginTop: "59px",
       }}
     >
       <div>
@@ -31,17 +34,17 @@ export default function Contact() {
       <div
         class="contact-mid"
         style={{
-          marginLeft: "25%",
-          marginRight: "25%",
+          marginLeft: "15%",
+          marginRight: "15%",
           marginTop: "1%",
           marginBottom: "1%",
-          backgroundColor: "#3c879e",
+          backgroundColor: "#f9f9f9",
         }}
       >
-        <div style={{ margin: "1%" }}>
-          <Grid>
-            <GridRow>
-              <GridColumn width={10}>
+        <div>
+          <Container>
+            <Row>
+              <Col md={6}>
                 <h2>Please fill in your details</h2>
                 <Form>
                   <FormGroup widths={2}>
@@ -56,44 +59,59 @@ export default function Contact() {
                   <FormInput width={16} placeholder="Your Message" />
                   <FormCheckbox label="I agree to the Terms and Conditions" />
                   <ButtonGroup widths="3">
-                    <Button type="submit" color="olive">
+                    <Button type="submit" color="linkedin">
                       Send
                     </Button>
                   </ButtonGroup>
                 </Form>
-              </GridColumn>
-              <GridColumn width={6}>
+              </Col>
+              <Col md={6}>
                 <h2>HealTrip</h2>
                 <div class="location">
                   <FontAwesomeIcon
                     icon={faMapMarkerAlt}
-                    color="white" 
-                    style={{ fontSize: '1.2em' }}
+                    color="#1f88be"
+                    style={{ fontSize: "1.2em" }}
                   />
                   <span>
-                    Çıplaklı, Akdeniz Blv. No:290/A, 07190{" "}
-                    <span>Döşemealtı/Antalya</span>
+                    Çıplaklı, Akdeniz Blv. No:290/A, 07190 Döşemealtı/Antalya
                   </span>
                 </div>
-                <div class="phone" style={{marginTop:"2%"}}>
-                  <FontAwesomeIcon icon={faPhone} color="white"/>
-                  <span>+90 554 382 22 15</span>                
-                </div>
-                <div class="phone" style={{marginTop:"2%"}}>
-                  <FontAwesomeIcon icon={faWhatsapp} color="white" style={{ fontSize: '1.3em' }}/>
+                <div class="phone" style={{ marginTop: "2%" }}>
+                  <FontAwesomeIcon icon={faPhone} color="#1f88be" />
                   <span>+90 554 382 22 15</span>
                 </div>
-                <div class="mail" style={{marginTop:"2%"}}>
-                  <FontAwesomeIcon icon={faEnvelope} color="white" style={{ fontSize: '1.1em' }}/>
+                <div class="phone" style={{ marginTop: "2%" }}>
+                  <FontAwesomeIcon
+                    icon={faWhatsapp}
+                    color="#1f88be"
+                    style={{ fontSize: "1.3em" }}
+                  />
+                  <span>+90 554 382 22 15</span>
+                </div>
+                <div class="mail" style={{ marginTop: "2%" }}>
+                  <FontAwesomeIcon
+                    icon={faEnvelope}
+                    color="#1f88be"
+                    style={{ fontSize: "1.1em" }}
+                  />
                   <span>ÖMÜRBABA@healtrip.com</span>
                 </div>
-              </GridColumn>
-            </GridRow>
-          </Grid>
-          <div style={{marginTop:"2%", marginBottom:"2%"}}>
-            <MapContainer />
-          </div>
+              </Col>
+            </Row>
+          </Container>
         </div>
+      </div>
+      <div class="gmap">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3184.2356482233486!2d30.618362076305235!3d37.051867253471976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14c39abd6e49aa8d%3A0x329b9c7bc561e87a!2sAntalya%20Bilim%20%C3%9Cniversitesi!5e0!3m2!1str!2str!4v1711130859737!5m2!1str!2str"
+          width="600"
+          height="450"
+          style={{border:"0", width:"70%"}}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
     </div>
   );
