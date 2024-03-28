c8f662152f521c2fab4cfd83708142c3a79ca92d
burakonurerten <burakonurerten@gmail.com>
2024-03-28 16:07:24 +0300

    make more reponsive gallery section

--- src/pages/home/MyGallery.js
+++ src/pages/home/MyGallery.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "./MyGallery.css";
 import photo1 from "./galleryImages/gallery-1.jpg";
@@ -11,36 +11,44 @@ import photo7 from "./galleryImages/gallery-7.jpg";
 import photo8 from "./galleryImages/gallery-8.jpg";
 
 export const MyGallery = () => {
-  const data = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8];
+  const images = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8];
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <div style={{ backgroundColor: "#3c879e", height: "100%", padding:"40px 0"}}>
-        <h1 style={{textAlign:"center", color:"#fff", padding:"4px", fontFamily:"Oswald, sans-serif", fontSize:"2.5rem"}}>GALLERY</h1>
-      <Carousel
-        data-bs-theme="primary"
-        style={{margin: "0 auto", width:"90%"}}
-      >
-        <Carousel.Item>
-        <div className="d-flex justify-content-center">
-        <img className="d-block w-25" src={data[0]} alt="First slide" style={{marginRight:"30px"}}/>
-        <img className="d-block w-25" src={data[1]} alt="Second slide" style={{marginRight:"30px"}}/>
-        <img className="d-block w-25" src={data[2]} alt="Third slide" />
-      </div>
-        </Carousel.Item>
-        <Carousel.Item>
-        <div className="d-flex justify-content-center">
-        <img className="d-block w-25" src={data[3]} alt="Second slide" style={{marginRight:"30px"}}/>
-        <img className="d-block w-25" src={data[4]} alt="Second slide" style={{marginRight:"30px"}}/>
-        <img className="d-block w-25" src={data[5]} alt="Second slide" />
-        </div>
-        </Carousel.Item>
-        <Carousel.Item>
-        <div className="d-flex justify-content-center">
-          <img className="d-block w-25" src={data[6]} alt="Third slide" style={{marginRight:"30px"}} />
-          <img className="d-block w-25" src={data[7]} alt="Third slide" style={{marginRight:"30px"}}/>
-          <img className="d-block w-25" src={data[3]} alt="Third slide" />
-        </div>
-        </Carousel.Item>
+    <div style={{ backgroundColor: "#3c879e", height: "100%", padding: "40px 0" }}>
+      <h1 style={{ textAlign: "center", color: "#fff", padding: "4px", fontFamily: "Oswald, sans-serif", fontSize: "2.5rem" }}>GALLERY</h1>
+      <Carousel data-bs-theme="primary" style={{ margin: "0 auto", width: "90%" }}>
+        {windowWidth > 900 ? (
+          [...Array(Math.ceil(images.length / 3))].map((_, slideIndex) => (
+            <Carousel.Item key={slideIndex}>
+              <div className="d-flex justify-content-center">
+                {images.slice(slideIndex * 3, (slideIndex + 1) * 3).map((image, index) => (
+                  <img className="d-block w-25" src={image} alt={`Slide ${slideIndex * 3 + index + 1}`} style={{ marginRight: "30px" }} key={index} />
+                ))}
+              </div>
+            </Carousel.Item>
+          ))
+        ) : (
+          images.map((image, index) => (
+            <Carousel.Item key={index}>
+              <div className="d-flex justify-content-center">
+                <img className="d-block" src={image} alt={`Slide ${index + 1}`} />
+              </div>
+            </Carousel.Item>
+          ))
+        )}
       </Carousel>
     </div>
   );
