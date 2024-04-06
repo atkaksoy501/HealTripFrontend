59d0f5258dc6ae915c3bb566e603772708984747
Aziz Alp Yolcu <azizalpyolcu09@outlook.com>
2024-04-06 20:43:46 +0300

    XD

--- src/pages/treatments/TreatmentDetail.js
+++ src/pages/treatments/TreatmentDetail.js
@@ -1,10 +1,6 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 
 export default function TreatmentDetail() {
-  const location = useLocation();
-  console.log('Location state:', location.state);
-  const { treatment } = location.state || {};
 
   return (
     <div style={{ marginTop: "80px" }}>
