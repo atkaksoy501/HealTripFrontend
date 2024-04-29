30e469b44f43d8d5bf122c608d04cf9ba0f810ce
burakonurerten <burakonurerten@gmail.com>
2024-04-29 16:39:18 +0300

    style forms for patient

--- src/layouts/GetMyProfile.js
+++ src/layouts/GetMyProfile.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { ButtonGroup, Button } from "reactstrap";
 import { jwtDecode } from "jwt-decode";
-import axios from 'axios';
+import axios from "axios";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import HealingIcon from "@mui/icons-material/Healing";
+import DescriptionIcon from '@mui/icons-material/Description';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 export default function GetMyProfile() {
-
   const [patientForms, setPatientForms] = useState([]);
   const [patient, setPatient] = useState(null);
-  const [showForms, setShowForms] = useState(false); 
+  const [showForms, setShowForms] = useState(false);
 
   useEffect(() => {
     const userToken = localStorage.getItem("token");
@@ -21,13 +24,15 @@ export default function GetMyProfile() {
     const fetchPatientForms = async () => {
       try {
         if (patient) {
-          const response = await axios.get(`https://healtrip.azurewebsites.net/patient/get/${patient.id}`);
+          const response = await axios.get(
+            `https://healtrip.azurewebsites.net/patient/get/${patient.id}`
+          );
           setPatientForms(response.data.bookings);
         }
-      } catch(error) {
+      } catch (error) {
         console.log(error);
       }
-    }
+    };
     fetchPatientForms();
   }, [patient]);
 
@@ -50,17 +55,122 @@ export default function GetMyProfile() {
           borderColor: "#fff",
         }}
       >
-        <Button className="custom-button" onClick={handleShowForms}>My Forms</Button>
-        <Button className="custom-button" onClick={handleShowProfile}>Profile Settings</Button>
+        <Button className="custom-button" onClick={handleShowForms}>
+          My Forms
+        </Button>
+        <Button className="custom-button" onClick={handleShowProfile}>
+          Profile Settings
+        </Button>
       </ButtonGroup>
 
       {showForms ? (
-        patientForms && patientForms.map(form => (
-          <div key={form.id}>
-            <p>{form.hospital.hospitalName}</p>
-            <p>{form.retreat.retreat_name}</p>
-            <p>{form.description}</p>
-            <p>----------------------------------------</p>
+        patientForms &&
+        patientForms.map((form) => (
+          <div key={form.id} style={{ marginTop: "30px" }}>
+            <li
+              key={form.id}
+              className="list-group-item"
+              style={{
+                border: "1px solid #ccc",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                width: "80%",
+                padding: "10px",
+                margin: "auto",
+              }}
+            >
+              <div>
+                <div style={{ display: "flex", color: "#295d6d" }}>
+                  <div style={{ display: "flex", paddingTop: "3px" }}>
+                    <LocalHospitalIcon style={{ color: "#295d6d" }} />
+                  </div>
+                  <div style={{ display: "flex" }}>
+                    <h3 style={{ fontWeight: "1000", fontSize: "20px" }}>
+                      Hospital Name:{" "}
+                    </h3>
+                    <p
+                      style={{
+                        marginTop: "6px",
+                        marginLeft: "6px",
+                        fontWeight: "bold",
+                        color: "#3c879e",
+                      }}
+                    >
+                      {form.hospital.hospitalName}
+                    </p>
+                  </div>
+                </div>
+
+                <div style={{ display: "flex", color: "#295d6d" }}>
+                  <div style={{ display: "flex", paddingTop: "3px" }}>
+                    <HealingIcon style={{ color: "#295d6d" }} />
+                  </div>
+                  <div style={{ display: "flex" }}>
+                    <h3 style={{ fontWeight: "1000", fontSize: "20px" }}>
+                      Retreat Name:{" "}
+                    </h3>
+
+                    <p
+                      style={{
+                        marginTop: "6px",
+                        marginLeft: "6px",
+                        fontWeight: "bold",
+                        color: "#3c879e",
+                      }}
+                    >
+                      {form.retreat.retreat_name}
+                    </p>
+                  </div>
+                </div>
+
+                <div style={{ display: "flex", color: "#295d6d" }}>
+                  <div style={{ display: "flex", paddingTop: "3px" }}>
+                    <DescriptionIcon style={{ color: "#295d6d" }} />
+                  </div>
+                  <div style={{ display: "flex" }}>
+                    <h3 style={{ fontWeight: "1000", fontSize: "20px" }}>
+                      Retreat Name:{" "}
+                    </h3>
+
+                    <p
+                      style={{
+                        marginTop: "6px",
+                        marginLeft: "6px",
+                        fontWeight: "bold",
+                        color: "#3c879e",
+                      }}
+                    >
+                      {form.description}
+                    </p>
+                  </div>
+                </div>                
+                
+                <div style={{ display: "flex", color: "#295d6d" }}>
+                  <div style={{ display: "flex", paddingTop: "3px" }}>
+                    <CalendarMonthIcon style={{ color: "#295d6d" }} />
+                  </div>
+                  <div style={{ display: "flex" }}>
+                    <h3 style={{ fontWeight: "1000", fontSize: "20px" }}>
+                      Submission Date:{" "}
+                    </h3>
+
+                    <p
+                      style={{
+                        marginTop: "6.5px",
+                        marginLeft: "6px",
+                        fontWeight: "bold",
+                        color: "#3c879e",
+                      }}
+                    >
+                      {form.bookingDate}
+                    </p>
+                  </div>
+                </div>
+
+
+              </div>
+            </li>
           </div>
         ))
       ) : (
