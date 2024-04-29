bd3d95c0c794ea331530e77fd677ed3d1e4e71df
burakonurerten <burakonurerten@gmail.com>
2024-04-29 11:24:15 +0300

    add buttons to profile section

--- src/layouts/GetMyProfile.js
+++ src/layouts/GetMyProfile.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { ButtonGroup, Button } from "reactstrap";
+
 
 export default function GetMyProfile() {
   return (
-    <div style={{marginTop:"59px"}}><h1>GetMyProfile</h1></div>
+    <div style={{marginTop:"100px", marginBottom:"50px"}}>
+    <ButtonGroup className="button-container" style={{width:"60%",display:"flex", margin:"auto", borderColor: "#fff"}}>
+        <Button className="custom-button">My Forms</Button>
+        <Button className="custom-button">Profile Settings</Button>
+    </ButtonGroup>
+  </div>
   )
 }
