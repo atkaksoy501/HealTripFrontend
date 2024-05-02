9166674d770801ce65efd32a2ffe39d377063d86
burakonurerten <burakonurerten@gmail.com>
2024-05-02 21:50:33 +0300

    add chat bot to get medical advice

--- src/layouts/GetMedicalAdvice.js
+++ src/layouts/GetMedicalAdvice.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export default function GetMedicalAdvice() {
   return (
-    <div style={{marginTop:"100px"}}><h1>GetMedicalAdvice</h1></div>
-  )
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', marginTop:"100px", marginBottom:"40px"}}>
+      <iframe
+        src="https://healtripai.onrender.com/?embed=true&pass=ss32_817"
+        title="Embedded Content"
+        frameBorder="0"
+        style={{
+          flex: 1,
+          overflow: 'hidden',
+          overflowX: 'hidden',
+          overflowY: 'hidden',
+          position: 'relative',
+          padding:"0 10%",
+        }}
+        width="100%"
+      />
+
+    </div>
+  );
 }
