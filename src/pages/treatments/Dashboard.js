49d9437590a8e9709eb14b2977258963b62fc963
burakonurerten <burakonurerten@gmail.com>
2024-05-07 17:54:55 +0300

    style the treatments

--- src/pages/treatments/Dashboard.js
+++ src/pages/treatments/Dashboard.js
@@ -4,6 +4,7 @@ import Treatments from './Treatments'
 // eslint-disable-next-line
 import { Container} from 'semantic-ui-react'
 import "./Dashboard.css";
+import dahsboard_background from "./blog6.jpg"
 
 
 export default function Dashboard() {
@@ -16,7 +17,11 @@ export default function Dashboard() {
   };
 
   return (
-    <div style={{marginTop:"80px"}}>
+    <div style={{ marginTop: "59px" }}>
+      <div className="section-title" style={{ backgroundImage: `url(${dahsboard_background})` }}>
+        <h1 style={{ textAlign: "center", color: "#295D6D", fontSize: "4.5rem", fontWeight: "bold", fontFamily: "'Oswald', sans-serif" }}>TREATMENTS</h1>
+        <p style={{ textAlign: "center", color: "#295D6D", fontSize: "1.1rem", fontWeight: "700" }}>Welcome to our treatments page! <br />Explore a world of relaxation as we offer a range of luxurious treatments designed to pamper your body and soothe your mind.</p>
+      </div>
       <Container className="main">
         <Departments onDepartmentClick={handleDepartmentClick} />
         <Treatments departmentId={selectedDepartmentId} departmentName={selectedDepartmentName} />
